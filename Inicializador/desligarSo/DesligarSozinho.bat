6ddfc69b57934ec1430bac3c29b4594bf0ab4561
marcosr1 <marcosrichelly43@gmail.com>
2025-05-12 20:51:13 -0300

    Inicializador

---
+++ Inicializador/desligarSo/DesligarSozinho.bat
@@ -0,0 +1,2 @@
+@echo off
+java --module-path %PATH_TO_FX% --add-modules javafx.controls,javafx.fxml -cp Desligamento1.jar application.Main 
---
+++ Inicializador/desligarSo/iniciar_escondido.vbs
@@ -0,0 +1,2 @@
+Set WshShell = CreateObject("WScript.Shell")
+WshShell.Run "DesligarSozinho.bat", 0, False
